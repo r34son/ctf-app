7e1710d16e3d03ba8b4832c60c96ffbc43723518
Yahub <seitasanov.yahub@mail.ru>
2019-10-27 18:10:28 +0300

    more fixes

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -23,9 +23,9 @@ const useStyles = makeStyles(theme => ({
     },
     paper: {
         backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
+        //border: '2px solid #000',
         boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
+        padding: theme.spacing(3, 4, 3),
     },
     grid: {
         outline: '0',
